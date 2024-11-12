56db6fe44f41f40a09670fd1f171425bb0c0a0b1
ChrisWilson <jzm@outlook.sk>
2024-11-12 10:33:12 +0800

    模型配置

--- src/config/modelProviders/openai.ts
+++ src/config/modelProviders/openai.ts
@@ -544,16 +544,10 @@ const OpenAI: ModelProviderCard = {
       vision: false,
     },
     {
-      displayName: 'deepseek-coder-6.7b-base-awq',
+      displayName: 'deepseek-coder-6.7b-instruct-awq',
       enabled: true,
-      id: '@hf/thebloke/deepseek-coder-6.7b-base-awq',
+      id: '@hf/thebloke/deepseek-coder-6.7b-instruct-awq',
       tokens: 16_384,
-    },
-    {
-      displayName: 'deepseek-math-7b-instruct',
-      enabled: true,
-      id: '@hf/thebloke/deepseek-math-7b-instruct',
-      tokens: 4096,
     },
     {
       displayName: 'gemma-7b-it',
@@ -562,19 +556,38 @@ const OpenAI: ModelProviderCard = {
       tokens: 2048,
     },
     {
+      displayName: 'hermes-2-pro-mistral-7b',
+      enabled: true,
+      // functionCall: true,
+      id: '@hf/nousresearch/hermes-2-pro-mistral-7b',
+      tokens: 4096,
+    },
+    {
       displayName: 'llama-3-8b-instruct-awq',
       id: '@cf/meta/llama-3-8b-instruct-awq',
       tokens: 8192,
     },
     {
-      displayName: 'llama-3.1-8b-instruct',
-      id: '@cf/meta/llama-3.1-8b-instruct',
-      tokens: 128_000,
-    },
-    {
       displayName: 'mistral-7b-instruct-v0.2',
       id: '@hf/mistral/mistral-7b-instruct-v0.2',
       tokens: 4096,
+    },
+    {
+      displayName: 'neural-chat-7b-v3-1-awq',
+      enabled: true,
+      id: '@hf/thebloke/neural-chat-7b-v3-1-awq',
+      tokens: 32_768,
+    },
+    {
+      displayName: 'openchat-3.5-0106',
+      id: '@cf/openchat/openchat-3.5-0106',
+      tokens: 8192,
+    },
+    {
+      displayName: 'openhermes-2.5-mistral-7b-awq',
+      enabled: true,
+      id: '@hf/thebloke/openhermes-2.5-mistral-7b-awq',
+      tokens: 32_768,
     },
     {
       displayName: 'qwen1.5-14b-chat-awq',
@@ -583,10 +596,24 @@ const OpenAI: ModelProviderCard = {
       tokens: 32_768,
     },
     {
-      displayName: 'qwen1.5-7b-chat-awq',
+      displayName: 'starling-lm-7b-beta',
       enabled: true,
-      id: '@cf/qwen/qwen1.5-7b-chat-awq',
+      id: '@hf/nexusflow/starling-lm-7b-beta',
+      tokens: 4096,
+    },
+    {
+      displayName: 'zephyr-7b-beta-awq',
+      enabled: true,
+      id: '@hf/thebloke/zephyr-7b-beta-awq',
       tokens: 32_768,
+    },
+    {
+      description:
+        'Generation over generation, Meta Llama 3 demonstrates state-of-the-art performance on a wide range of industry benchmarks and offers new capabilities, including improved reasoning.\t',
+      displayName: 'meta-llama-3-8b-instruct',
+      enabled: true,
+      functionCall: false,
+      id: '@hf/meta-llama/meta-llama-3-8b-instruct',
     },
     {
       displayName: 'bing-ai',
@@ -968,11 +995,15 @@ const OpenAI: ModelProviderCard = {
       vision: true,
     },
     {
-      description: 'Grok-Beta 马斯克的测试模型',
-      displayName: 'Grok-Beta',
+      description: '拥有与 Grok 2 相当的性能，但具有更高的效率、速度和功能。',
+      displayName: 'Grok Beta',
       enabled: true,
       functionCall: true,
       id: 'grok-beta',
+      pricing: {
+        input: 5,
+        output: 15,
+      },
       tokens: 131_072,
     },
   ],
