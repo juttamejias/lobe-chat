a0d293dbf717d115a29265e6e1305c4a5a94810a
ChrisWilson <jzm@outlook.sk>
2025-01-21 09:58:34 +0800

    add deepseek 思考模型

--- src/config/modelProviders/openai.ts
+++ src/config/modelProviders/openai.ts
@@ -362,18 +362,33 @@ const OpenAI: ModelProviderCard = {
     {
       contextWindowTokens: 65_536,
       description:
-        '融合通用与代码能力的全新开源模型, 不仅保留了原有 Chat 模型的通用对话能力和 Coder 模型的强大代码处理能力，还更好地对齐了人类偏好。此外，DeepSeek-V3 在写作任务、指令跟随等多个方面也实现了大幅提升。',
+        '最新模型 DeepSeek-V3 多项评测成绩超越 Qwen2.5-72B 和 Llama-3.1-405B 等开源模型，性能对齐领军闭源模型 GPT-4o 与 Claude-3.5-Sonnet。',
       displayName: 'DeepSeek V3',
       enabled: true,
       functionCall: true,
       id: 'deepseek-chat',
-      pricing: {
+      pricing: { // 2025.2.9 之后涨价
         cachedInput: 0.1,
         currency: 'CNY',
-        input: 2,
-        output: 8,
+        input: 1,
+        output: 2,
       },
-      releasedAt: '2024-09-05',
+      releasedAt: '2024-12-26',
+    },
+    {
+      contextWindowTokens: 65_536,
+      description:
+        'DeepSeek 推出的推理模型。在输出最终回答之前，模型会先输出一段思维链内容，以提升最终答案的准确性。',
+      displayName: 'DeepSeek R1',
+      enabled: true,
+      id: 'deepseek-reasoner',
+      pricing: {
+        cachedInput: 1,
+        currency: 'CNY',
+        input: 4,
+        output: 16,
+      },
+      releasedAt: '2025-01-20',
     },
     // claude
     {
